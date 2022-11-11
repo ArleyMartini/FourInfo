6305b40b6fcabf8b8bf19b629022170a69a65428
ArleyMartini <arley.assis.03@gmail.com>
2022-11-11 20:00:55 -0300

    Project Create

---
+++ backend/src/test/java/PW12022/fourinfo/FourinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package PW12022.fourinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
